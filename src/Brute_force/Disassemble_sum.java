c2ded15ce49f468f31bcf0d3d31317167cabb7e8
hayung <yoohayung10309@gmail.com>
2023-11-03 00:02:50 +0900

    분해합 _ 맞은 내용

--- src/Brute_force/Disassemble_sum.java
+++ src/Brute_force/Disassemble_sum.java
@@ -11,37 +11,36 @@ public class Disassemble_sum {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
         int num = Integer.parseInt(br.readLine());
         String Num_str = Integer.toString(num);
         int num_length = Num_str.length();
-        int constructor =1 ; // 생성자
+        // 생성자
         ArrayList<Integer> result = new ArrayList<Integer>();
-        
-       
 
-        while (true) {
+        for (int constructor = 0; constructor < num; constructor++) {
             int sum = 0;
+            int number = constructor;
             String strNum = Integer.toString(constructor);
+
             int[] arrNum = new int[strNum.length()];
-            
+
             for (int i = 0; i < strNum.length(); i++) {
                 arrNum[i] = strNum.charAt(i) - '0';
                 sum = sum + arrNum[i];
-                
             }
-            
+
 
             sum = sum + constructor;
             if (sum == num) {
                 result.add(constructor);
-                bw.write(constructor + " ");
-                break;
-            }else if (sum > num){
+                bw.write( result.get(0) + " ");
                 break;
             }
-            constructor++;
+            
         }
-        if(result.size() ==0){
+        if (result.size() == 0) {
             bw.write("0\n");
         }
         bw.flush();
