ed6124ecfa2090e3a571bc1408e9d2d32cb55474
hayung <yoohayung10309@gmail.com>
2024-01-30 23:02:26 +0900

    1.30(화) 백준 1463문제

--- src/약수배수와소수2/Making1_1463.java
+++ src/약수배수와소수2/Making1_1463.java
@@ -3,46 +3,36 @@ package 약수배수와소수2;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
+
 
 public class Making1_1463 {
     static int count = 0;
-    static ArrayList<Integer> list = new ArrayList<>();
+    static int[] memo;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         int num = Integer.parseInt(br.readLine());
-
-        if (num == 1) {
-            list.add(count);
-        } else {
-            shrink_num(num);
-        }
-        Collections.sort(list);
-        sb.append(list.get(0));
-        System.out.println(sb);
+        memo = new int[num+1];
+        System.out.println(shrink_num(num));
     }
 
-    private static void shrink_num(int num) {
+    private static int shrink_num(int num) {
         if (num == 1) {
-            list.add(count);
-        } else if (num % 3 == 0) {
-            count++;
-            shrink_num(num / 3);
-        } else if ((num % 3 == 1) && (num % 2 == 0)) {
-            count++;
-            shrink_num(num - 1);
-            shrink_num(num / 2);
-        } else if (num % 2 == 0) {
-            count++;
-            shrink_num(num / 2);
-        } else {
-
-            count++;
-            shrink_num(num - 1);
+            return 0;
         }
-
+        if (memo[num] == 0) {
+            if ((num % 3 == 0) && (num % 2 == 0)) {
+                memo[num] = Math.min(shrink_num(num / 3) + 1, shrink_num(num / 2) + 1);
+                return memo[num] = Math.min(memo[num], shrink_num(num - 1) + 1);
+            } else if (num % 3 == 0) {
+                return memo[num] = Math.min(shrink_num(num / 3) + 1, shrink_num(num - 1) + 1);
+            } else if (num % 2 == 0) {
+                return memo[num] = Math.min(shrink_num(num / 2) + 1, shrink_num(num - 1) + 1);
+            } else {
+                return memo[num] = shrink_num(num - 1) + 1;
+            }
+        } else {
+            return memo[num];
+        }
     }
 }
